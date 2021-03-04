9e48259b854332c89d33356bba78616088ca2cb2
Rasoul678 <h.rostami.r@gmail.com>
2021-03-04 23:34:00 +0330

    Add Raaivan plugins to CKEditor

--- packages/ckeditor5-build-balloon-block/src/ckeditor.js
+++ packages/ckeditor5-build-balloon-block/src/ckeditor.js
@@ -4,39 +4,76 @@
  */
 
 // The editor creator to use.
-import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
+import BalloonEditorBase from "@ckeditor/ckeditor5-editor-balloon/src/ballooneditor";
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
+import Code from "@ckeditor/ckeditor5-basic-styles/src/code";
+import CodeBlock from "@ckeditor/ckeditor5-code-block/src/codeblock";
+import FontBackgroundColor from "@ckeditor/ckeditor5-font/src/fontbackgroundcolor";
+import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor";
+import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline";
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock";
+import ListStyle from "@ckeditor/ckeditor5-list/src/liststyle";
+import MediaEmbedToolbar from "@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar";
+import Mention from "@ckeditor/ckeditor5-mention/src/mention";
+import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat";
+import SimpleUploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter";
+import SpecialCharacters from "@ckeditor/ckeditor5-special-characters/src/specialcharacters";
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough";
+import Subscript from "@ckeditor/ckeditor5-basic-styles/src/subscript";
+import Superscript from "@ckeditor/ckeditor5-basic-styles/src/superscript";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline";
+import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
+import TableProperties from "@ckeditor/ckeditor5-table/src/tableproperties";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
+import BlockToolbar from "@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
+import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
+import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading";
+import Image from "@ckeditor/ckeditor5-image/src/image";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent";
+import Link from "@ckeditor/ckeditor5-link/src/link";
+import List from "@ckeditor/ckeditor5-list/src/list";
+import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+import Table from "@ckeditor/ckeditor5-table/src/table";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
 
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-
-import '../theme/theme.css';
+import "../theme/theme.css";
 
 export default class BalloonEditor extends BalloonEditorBase {}
 
 // Plugins to include in the build.
 BalloonEditor.builtinPlugins = [
+	Alignment,
+	Code,
+	CodeBlock,
+	FontBackgroundColor,
+	FontColor,
+	HorizontalLine,
+	IndentBlock,
+	ListStyle,
+	MediaEmbedToolbar,
+	Mention,
+	RemoveFormat,
+	SimpleUploadAdapter,
+	SpecialCharacters,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Underline,
+	TableCellProperties,
+	TableProperties,
 	Essentials,
 	UploadAdapter,
 	Autoformat,
@@ -60,50 +97,55 @@ BalloonEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
 ];
 
 // Editor configuration.
 BalloonEditor.defaultConfig = {
 	blockToolbar: [
-		'heading',
-		'|',
-		'bulletedList',
-		'numberedList',
-		'|',
-		'indent',
-		'outdent',
-		'|',
-		'imageUpload',
-		'blockQuote',
-		'insertTable',
-		'mediaEmbed',
-		'|',
-		'undo',
-		'redo'
+		"heading",
+		"|",
+		"fontBackgroundColor",
+		"fontColor",
+		"|",
+		"horizontalLine",
+		"code",
+		"codeBlock",
+		"blockQuote",
+		"specialCharacters",
+		"|",
+		"alignment",
+		"indent",
+		"outdent",
+		"bulletedList",
+		"numberedList",
+		"removeFormat",
+		"strikethrough",
+		"underline",
+		"subscript",
+		"superscript",
+		"|",
+		"imageUpload",
+		"mediaEmbed",
+		"insertTable",
+		"|",
+		"undo",
+		"redo",
 	],
 	toolbar: {
-		items: [
-			'bold',
-			'italic',
-			'link'
-		]
+		items: ["bold", "italic", "link"],
 	},
 	image: {
 		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
+			"imageStyle:full",
+			"imageStyle:side",
+			"|",
+			"imageTextAlternative",
+		],
 	},
 	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
+		contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: "en",
 };
