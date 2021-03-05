8fd9672b4a292ac878c6617afa286b3f75459660
Rasoul678 <h.rostami.r@gmail.com>
2021-03-05 16:28:34 +0330

    Add simple mention with some items

--- packages/ckeditor5-build-balloon-block/src/ckeditor.js
+++ packages/ckeditor5-build-balloon-block/src/ckeditor.js
@@ -53,6 +53,68 @@ import "../theme/theme.css";
 
 export default class BalloonEditor extends BalloonEditorBase {}
 
+const items = [
+	{
+		id: "@swarley",
+		userId: "1",
+		name: "Barney Stinson",
+		link: "https://www.imdb.com/title/tt0460649/characters/nm0000439",
+	},
+	{
+		id: "@lilypad",
+		userId: "2",
+		name: "Lily Aldrin",
+		link: "https://www.imdb.com/title/tt0460649/characters/nm0004989",
+	},
+	{
+		id: "@marshmallow",
+		userId: "3",
+		name: "Marshall Eriksen",
+		link: "https://www.imdb.com/title/tt0460649/characters/nm0781981",
+	},
+	{
+		id: "@rsparkles",
+		userId: "4",
+		name: "Robin Scherbatsky",
+		link: "https://www.imdb.com/title/tt0460649/characters/nm1130627",
+	},
+	{
+		id: "@tdog",
+		userId: "5",
+		name: "Ted Mosby",
+		link: "https://www.imdb.com/title/tt0460649/characters/nm1102140",
+	},
+];
+
+const getFeedItems = (queryText) => {
+	// As an example of an asynchronous action, return a promise
+	// that resolves after a 100ms timeout.
+	// This can be a server request or any sort of delayed action.
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			const itemsToDisplay = items
+				// Filter out the full list of all items to only those matching the query text.
+				.filter(isItemMatching)
+				// Return 10 items max - needed for generic queries when the list may contain hundreds of elements.
+				.slice(0, 10);
+
+			resolve(itemsToDisplay);
+		}, 100);
+	});
+
+	// Filtering function - it uses the `name` and `username` properties of an item to find a match.
+	function isItemMatching(item) {
+		// Make the search case-insensitive.
+		const searchString = queryText.toLowerCase();
+
+		// Include an item in the search results if the name or username includes the current user input.
+		return (
+			item.name.toLowerCase().includes(searchString) ||
+			item.id.toLowerCase().includes(searchString)
+		);
+	}
+};
+
 // Plugins to include in the build.
 BalloonEditor.builtinPlugins = [
 	Alignment,
@@ -132,6 +194,14 @@ BalloonEditor.defaultConfig = {
 		"undo",
 		"redo",
 	],
+	mention: {
+		feeds: [
+			{
+				marker: "@",
+				feed: getFeedItems,
+			},
+		],
+	},
 	toolbar: {
 		items: ["bold", "italic", "link"],
 	},
